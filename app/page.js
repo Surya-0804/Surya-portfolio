1c54b02fa4a6ace6520bd08f3900218b50f87559
Surya-0804 <surya.abothula@gmail.com>
2025-02-16 20:08:46 +0530

    `Added Encryption component and updated import statements in page.js and Skills.jsx`

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
-import Hero from "@/components/main/Hero";
-import Skills from "@/components/main/Skills";
-import Image from "next/image";
+import Encryption from '@/components/main/Encryption';
+import Hero from '@/components/main/Hero';
+import Skills from '@/components/main/Skills';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <Encryption />
       </div>
     </main>
   );
--- components/main/Skills.jsx
+++ components/main/Skills.jsx
@@ -4,18 +4,18 @@ import {
   Full_stack,
   Other_skill,
   Skill_data,
-} from "@/constants";
-import React from "react";
-import SkillDataProvider from "../sub/SkillDataProvider";
-import Image from "next/image";
-import SkillText from "../sub/SkillText";
+} from '@/constants';
+import React from 'react';
+import SkillDataProvider from '../sub/SkillDataProvider';
+import Image from 'next/image';
+import SkillText from '../sub/SkillText';
 
 const Skills = () => {
   return (
     <section
       id="skills"
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
-      style={{ transform: "scale(0.9" }}
+      style={{ transform: 'scale(0.9)' }}
     >
       <SkillText />
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
