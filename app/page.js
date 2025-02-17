452b60d2c1fecaca861b0fe1acb5445e265ec424
Surya-0804 <surya.abothula@gmail.com>
2025-02-17 20:46:03 +0530

    Experience section is added

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Encryption from '@/components/main/Encryption';
+import Experience from '@/components/main/Experience';
 import Hero from '@/components/main/Hero';
 import Projects from '@/components/main/Projects';
 import Skills from '@/components/main/Skills';
@@ -8,10 +9,12 @@ export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
+        {/**Adding comment */}
         <Hero />
         <Skills />
         <Encryption />
         <Projects />
+        <Experience />
       </div>
     </main>
   );
---
+++ constants/experience.js
@@ -0,0 +1,81 @@
+export const experience = [
+  {
+    title: 'Dec 2024 - Present',
+    content: (
+      <div className="rounded-lg shadow-lg border border-[#2A0E61] bg-[#1A1A2E] cursor-pointer p-4">
+        <h3 className="text-white text-lg md:text-xl font-semibold mb-2">
+          Full-Stack Developer | CareAutomate
+        </h3>
+        <ul className="list-disc list-inside text-white text-xs md:text-sm font-normal space-y-2">
+          <li>
+            Spearheading the development of scalable web applications,
+            integrating modern front-end frameworks with robust back-end
+            architectures.
+          </li>
+          <li>
+            Collaborating with cross-functional teams to enhance automation
+            solutions and optimize software performance.
+          </li>
+          <li>
+            Implementing security best practices and CI/CD pipelines to
+            streamline deployment and maintain high application reliability.
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    title: 'Aug 2024 - Present',
+    content: (
+      <div className="rounded-lg shadow-lg border border-[#2A0E61] cursor-pointer p-4">
+        <h3 className="text-white text-lg md:text-xl font-semibold mb-2">
+          Teaching Aid | StudyOwl & Vishnu Institute of Technology
+        </h3>
+        <ul className="list-disc list-inside text-white text-xs md:text-sm font-normal space-y-2">
+          <li>
+            Assisting peers and juniors in mastering full-stack development,
+            focusing on technologies like React.js, Node.js, and Express.
+          </li>
+          <li>
+            Providing mentorship and guidance through coding exercises,
+            project-based learning, and hands-on labs to solidify understanding
+            of front-end and back-end concepts.
+          </li>
+          <li>
+            Organizing and conducting workshops and study sessions to help
+            students grasp full-stack development concepts and best practices.
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    title: 'Aug 2024 - Present',
+    content: (
+      <div className="rounded-lg shadow-lg border border-[#2A0E61] cursor-pointer p-4">
+        <h3 className="text-white text-lg md:text-xl font-semibold mb-2">
+          Community Manager & ML/DL Developer | Nexus Sparks
+        </h3>
+        <ul className="list-disc list-inside text-white text-xs md:text-sm font-normal space-y-2">
+          <li>
+            Assisting team members and community members in learning and
+            mastering React.js through workshops, tutorials, and one-on-one
+            mentoring.
+          </li>
+          <li>
+            Engaging with the community to provide technical support, answer
+            questions, and foster a positive learning environment.
+          </li>
+          <li>
+            Providing feedback on ML and DL projects and helping others
+            troubleshoot issues and improve their code.
+          </li>
+          <li>
+            Organizing ML and DL learning sessions and promoting a culture of
+            continuous learning and skill development.
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+];
